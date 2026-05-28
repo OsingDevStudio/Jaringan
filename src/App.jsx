b5b46a900d0c15041812fa201429baae309a047b
OsingDevStudio <osingdevstudio@gmail.com>
2026-05-28 21:20:18 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Birthday from './pages/Birthday';
 import Ucapan from './pages/Ucapan';
 import Kenangan from './pages/Kenangan';
 import Hadiah from './pages/Hadiah';
+import Calculator from './pages/Calculator';
+import Timeline from './pages/Timeline'; // Pastikan sudah diimpor
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Birthday />} />
+        <Route path="/" element={<Calculator />} />
+        <Route path="/birthday" element={<Birthday />} />
+        <Route path="/timeline" element={<Timeline />} /> {/* Tambahkan ini */}
         <Route path="/ucapan" element={<Ucapan />} />
         <Route path="/kenangan" element={<Kenangan />} />
         <Route path="/hadiah" element={<Hadiah />} />
